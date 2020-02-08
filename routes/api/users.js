2f655e1680cf8c74679db339fb0729509962d586
mtleonardjr <mtleonardjr@gmail.com>
2020-02-08 17:13:18 -0500

    authentication added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
-//@route  GET api/users
-//@desc   Test route
+const { check, validationResult } = require('express-validator');
+//@route  POST api/users
+//@desc   Register User
 //@access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with at least six characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
@@ -11,6 +14,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
